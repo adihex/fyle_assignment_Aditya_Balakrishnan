ade4e6caffd6f494f3dd490c3e1468f3cd37ffb6
Aditya Balakrishnan <adihex@tutanota.com>
2024-06-03 22:53:50 +0530

    Almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,41 @@
 // Import our custom CSS
-import '../scss/styles.scss'
+import "../scss/styles.scss";
+
+import { Tab, initMDB } from "mdb-ui-kit";
+
+initMDB({ Tab });
+
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
+
+//======== tiny slider for slider-items-active
+tns({
+  autoplay: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true,
+  gutter: 0,
+  container: ".slider-items-active",
+  center: true,
+  nav: true,
+  navPosition: "bottom",
+  controls: true,
+  speed: 400,
+  controlsText: [
+    '<i class="lni lni-arrow-left-circle"></i>',
+    '<i class="lni lni-arrow-right-circle"></i>',
+  ],
+  responsive: {
+    0: {
+      items: 1,
+    },
+
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+  },
+});
+// Initialization for ES Users
